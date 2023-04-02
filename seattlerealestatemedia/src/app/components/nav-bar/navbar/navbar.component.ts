db802715726d0f27a364e4d1f83f3c952611fe64
Monkifoto LLc <monkifoto@gmail.com>
2023-04-02 16:19:06 -0700

    fixed mobile nav

--- seattlerealestatemedia/src/app/components/nav-bar/navbar/navbar.component.ts
+++ seattlerealestatemedia/src/app/components/nav-bar/navbar/navbar.component.ts
@@ -7,13 +7,33 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
-onNgInit(){
+  ngOnInit(){
  
+    const nav = document.querySelector(".primary-navigation")as HTMLButtonElement ;
+    const navToggle = document.querySelector(".mobile-nav-toggle")as HTMLButtonElement ;;
+    console.log(nav);
+    navToggle.addEventListener("click", () => {
+        console.log("nav - toggle  - click");
+        const visiblity = nav.getAttribute("data-visible");
+       
+        console.log("visibility: " + visiblity);
+        if (visiblity === "false") {
+            nav.setAttribute("data-visible", "true");
+            navToggle.setAttribute("aria-expanded", "true");
+        } else {
+            nav.setAttribute("data-visible", "false");
+            navToggle.setAttribute("aria-expanded", "false");
+        }
+
+    });
+
 }
 
 scrollTo(element: any): void {
   console.log(element);
   (document.getElementById(element) as HTMLElement).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
+
+
 
 }
